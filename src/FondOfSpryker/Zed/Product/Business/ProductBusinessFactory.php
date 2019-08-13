06f016a155a13c831e4af104b7d7cef63a388031
Markus Nörenberg <markus.noerenberg@fondofbags.com>
2019-08-13 15:32:24 +0200

    keep spaces in sku

--- src/FondOfSpryker/Zed/Product/Business/ProductBusinessFactory.php
+++ src/FondOfSpryker/Zed/Product/Business/ProductBusinessFactory.php
@@ -2,7 +2,10 @@
 
 namespace FondOfSpryker\Zed\Product\Business;
 
+use FondOfSpryker\Zed\Product\Business\Product\Sku\FondOfSkuGenerator;
 use Spryker\Zed\Product\Business\Product\ProductAbstractManager;
+use Spryker\Zed\Product\Business\Product\Sku\SkuGeneratorInterface;
+use Spryker\Zed\Product\Business\Product\Url\ProductUrlGeneratorInterface;
 use Spryker\Zed\Product\Business\ProductBusinessFactory as BaseProductBusinessFactory;
 
 /**
@@ -11,9 +14,9 @@ use Spryker\Zed\Product\Business\ProductBusinessFactory as BaseProductBusinessFa
 class ProductBusinessFactory extends BaseProductBusinessFactory
 {
     /**
-     * @return \FondOfSpryker\Zed\Product\Business\ProductUrlGenerator
+     * @return \Spryker\Zed\Product\Business\Product\Url\ProductUrlGeneratorInterface
      */
-    public function createProductUrlGenerator()
+    public function createProductUrlGenerator(): ProductUrlGeneratorInterface
     {
         return new ProductUrlGenerator(
             $this->createProductAbstractNameGenerator(),
@@ -42,7 +45,7 @@ class ProductBusinessFactory extends BaseProductBusinessFactory
      *
      * @return \Spryker\Zed\Product\Business\Product\ProductAbstractManager
      */
-    protected function attachProductAbstractManagerObservers(ProductAbstractManager $productAbstractManager)
+    protected function attachProductAbstractManagerObservers(ProductAbstractManager $productAbstractManager): ProductAbstractManager
     {
         $productAbstractManager->attachBeforeCreateObserver($this->createProductAbstractBeforeCreateObserverPluginManager());
         $productAbstractManager->attachAfterCreateObserver($this->createProductAbstractAfterCreateObserverPluginManager());
@@ -51,5 +54,13 @@ class ProductBusinessFactory extends BaseProductBusinessFactory
         $productAbstractManager->attachReadObserver($this->createProductAbstractReadObserverPluginManager());
 
         return $productAbstractManager;
+    }
+
+    /**
+     * @return \Spryker\Zed\Product\Business\Product\Sku\SkuGeneratorInterface
+     */
+    public function createSkuGenerator(): SkuGeneratorInterface
+    {
+        return new FondOfSkuGenerator($this->getUtilTextService(), $this->createSkuIncrementGenerator());
     }
 }
