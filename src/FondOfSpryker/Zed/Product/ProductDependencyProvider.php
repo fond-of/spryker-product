e82909e1a299b537e78d1860e734008d45bba941
Julian Zimmermann <julian@jzit.de>
2022-10-19 16:39:22 +0200

    refactoring to fit latest changes from "spryker/product": "^6.29.0"

--- src/FondOfSpryker/Zed/Product/ProductDependencyProvider.php
+++ src/FondOfSpryker/Zed/Product/ProductDependencyProvider.php
@@ -2,7 +2,6 @@
 
 namespace FondOfSpryker\Zed\Product;
 
-use FondOfSpryker\Zed\Product\Dependency\Facade\ProductToStoreBridge;
 use FondOfSpryker\Zed\Product\Dependency\Facade\ProductToUrlBridge;
 use Spryker\Zed\Kernel\Container;
 use Spryker\Zed\Product\ProductDependencyProvider as BaseProductDependencyProvider;
@@ -10,7 +9,6 @@ use Spryker\Zed\Product\ProductDependencyProvider as BaseProductDependencyProvid
 class ProductDependencyProvider extends BaseProductDependencyProvider
 {
     public const FACADE_URL = 'PRODUCT:FACADE_URL';
-    public const FACADE_STORE = 'PRODUCT:FACADE_STORE';
 
     /**
      * @param \Spryker\Zed\Kernel\Container $container
@@ -21,7 +19,6 @@ class ProductDependencyProvider extends BaseProductDependencyProvider
     {
         $container = parent::provideBusinessLayerDependencies($container);
         $container = $this->addUrlFacade($container);
-        $container = $this->addStoreFacade($container);
 
         return $container;
     }
@@ -35,20 +32,6 @@ class ProductDependencyProvider extends BaseProductDependencyProvider
     {
         $container[static::FACADE_URL] = function (Container $container) {
             return new ProductToUrlBridge($container->getLocator()->url()->facade());
-        };
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    protected function addStoreFacade(Container $container): Container
-    {
-        $container[static::FACADE_STORE] = function (Container $container) {
-            return new ProductToStoreBridge($container->getLocator()->store()->facade());
         };
 
         return $container;
